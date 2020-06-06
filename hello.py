63991daa3123ad5cec3a6592ee05f03b79650803
Adrien Hong <adrienhong22@gmail.com>
2020-06-06 16:34:03 -0400

    added python script

---
+++ hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+  
+print "Content-type:text/html\n\n"
+print "<html>"
+print "<head><title>My First CGI Program</title></head>"
+print "<body>"
+print "<p> It works!!! </p>"
+for i in range(5):
+    print "<h1>Hello World!"
+print "</body>"
+print "</html>"
